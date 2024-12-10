2fb04a46eb3590d0c4a965897a070e134bcc2fd5
meryem <116671202+meryemorc@users.noreply.github.com>
2024-12-10 21:19:14 +0300

    .

--- demimarktt/demi/demi/urls.py
+++ demimarktt/demi/demi/urls.py
@@ -3,10 +3,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('homepage.urls')),  # Anasayfa uygulaması için
-    # Aşağıdaki satırları geçici olarak yorum satırına alın
-    # path('products/', include('products.urls')),
-    # path('cart/', include('cart.urls')),
-    # path('search/', include('search.urls')),
-    # path('account/', include('userAccount.urls')),
+    path('cart/', include('cart.urls')),
+    path('search/', include('search.urls')),
+    path('user/', include('userAccount.urls')),
+    path('products/', include('product.urls')),
+    path('', include('homepage.urls')), 
 ]
