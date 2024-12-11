aae469e3ec49fb247844fbfb721a16e5fb4bcee1
meryem <116671202+meryemorc@users.noreply.github.com>
2024-12-11 14:55:33 +0300

    .

--- demimarktt/demi/homepage/views.py
+++ demimarktt/demi/homepage/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render # type: ignore
 from django.http import HttpResponse # type: ignore
 
 def homepage(request):
-    return HttpResponse("Anasayfa")
+  return render(request, 'homepage/homepage.html')
 
 def about(request):
     return  HttpResponse("Hakkımızda")
