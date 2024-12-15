ef3debadc25163957cc3ca90980fc834430c0ac0
meryem <116671202+meryemorc@users.noreply.github.com>
2024-12-16 02:11:58 +0300

    brand filter

--- demimarktt/demi/homepage/views.py
+++ demimarktt/demi/homepage/views.py
@@ -2,14 +2,35 @@ from django.shortcuts import render, redirect  # type: ignore
 from django.http import HttpResponse  # type: ignore
 from cart.models import User  # type: ignore
 from products.models import Product  # type: ignore
+from django.core.paginator import Paginator # type: ignore
+
+from django.db.models import Count # type: ignore
 
 
 def homepage(request):
-    # MongoDB'den tüm ürünleri çek
+    # Marka filtresi için GET parametresini al
+    brand_filter = request.GET.get('brand')
+    
+    # Ürünleri filtrele
     products = Product.objects.all()
+    if brand_filter:  # Belirli bir marka seçilmişse ürünleri filtrele
+        products = products.filter(brand=brand_filter)
 
-    # Ürünleri template'e gönder
-    return render(request, 'homepage/homepage.html', {'products': products})
+    # Markaları ürün sayısına göre sıralayarak al
+    brands = Product.objects.values('brand').annotate(brand_count=Count('brand')).order_by('-brand_count')
+    
+    # Sayfalama: Her sayfada 20 ürün
+    paginator = Paginator(products, 20)
+    page_number = request.GET.get('page')
+    products_page = paginator.get_page(page_number)
+
+    return render(request, 'homepage/homepage.html', {
+        'products': products_page,
+        'brands': brands,
+    })
+
+
+
 
 
 def about(request):
--- demimarktt/demi/products/models.py
+++ demimarktt/demi/products/models.py
@@ -10,9 +10,10 @@ class Product(models.Model):
     price_before_discount = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     rating_out_of_5 = models.FloatField(null=True, blank=True)
     number_of_ratings = models.IntegerField(null=True, blank=True)
-
+    brand = models.CharField(max_length=50, null=True, blank=True)  # Brand alanı doğru tanımlanmış
+    
     class Meta:
-        db_table = "demiMarktBirlesik"  # Koleksiyon adı MongoDB'deki gibi olmalı
+        db_table = "demiMarktBirlesik"  # MongoDB koleksiyon adı doğru tanımlı
 
     def __str__(self):
         return self.product_name
